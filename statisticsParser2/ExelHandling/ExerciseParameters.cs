07abe3500a625c9dc96bc7e55f109994a038d957
edi4devop@gmail.com <edi4devop@gmail.com>
2021-08-08 08:40:13 +0300

    added

---
+++ statisticsParser2/ExelHandling/ExerciseParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statisticsParser2.ExelHandling
+{
+    class ExerciseParameters
+    {
+        string exerciseName;
+        string exexerciseType;
+        string exexerciseID;
+        protected Dictionary<String, ResultsClasses.IContestantResult> contestantsResults;
+
+    }
+}
